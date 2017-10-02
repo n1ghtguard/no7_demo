3c45006a27d10c72cad78b9c552791eea39c0452
n1ghtguard <nightguard322@gmail.com>
2017-10-02 22:47:09 +0300

    start_project

---
+++ sedona/less/scripts.js
@@ -0,0 +1,13 @@
+window.onload = function(){
+
+    var form = Document.querySelector('.site-form form');
+
+    Document.querySelector('.site-form .form-title').onclick = function(){
+
+        form.
+    }
+
+
+
+
+}
