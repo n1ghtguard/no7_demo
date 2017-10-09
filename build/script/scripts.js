22ffbbcf100c4e0d7d343f602a184a7372127705
n1ghtguard <nightguard322@gmail.com>
2017-10-09 11:51:20 +0300

    1st page ready

---
+++ build/script/scripts.js
@@ -0,0 +1,20 @@
+$(function() {
+
+    console.log(1);
+
+    $('.header-top__item--logo svg').on('click',function(){
+        console.log(1);
+    $('.header-top-menu li').slideToggle(300)
+    });
+
+    $('.footer-menu--main svg').on('click',function(){
+        console.log(1);
+        $('.footer-menu__items--main li').slideToggle(300)
+    });
+
+
+
+
+
+
+});
